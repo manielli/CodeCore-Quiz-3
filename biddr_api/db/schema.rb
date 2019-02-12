7708c35ac2cc02edc99eefd30cf96972b5df911b
Mani Jafari <manielli@gmail.com>
2019-02-12 10:58:32 -0800

    Added admin to users, set the default admin status to false in the migration file, ran rails db:migrate

--- biddr_api/db/schema.rb
+++ biddr_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_181942) do
+ActiveRecord::Schema.define(version: 2019_02_12_185657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_181942) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
